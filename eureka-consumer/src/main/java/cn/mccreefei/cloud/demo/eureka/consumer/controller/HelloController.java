40bb616365f5ff52728db822797b31d6f558812c
MccreeFei <mccreefei@foxmail.com>
2020-02-11 16:39:49 +0800

    config demo

--- eureka-consumer/src/main/java/cn/mccreefei/cloud/demo/eureka/consumer/controller/HelloController.java
+++ eureka-consumer/src/main/java/cn/mccreefei/cloud/demo/eureka/consumer/controller/HelloController.java
@@ -1,6 +1,7 @@
 package cn.mccreefei.cloud.demo.eureka.consumer.controller;
 
 import cn.mccreefei.cloud.demo.eureka.consumer.service.RemoteHelloService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,22 @@ import javax.annotation.Resource;
 public class HelloController {
     @Resource
     RemoteHelloService remoteHelloService;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${consumer.env}")
+    private String env;
     @GetMapping("/hello/{name}")
     public String sayHello(@PathVariable("name") String name) {
         return remoteHelloService.sayHello(name);
+    }
+
+    @GetMapping("/port")
+    public String getServerPort() {
+        return serverPort;
+    }
+
+    @GetMapping("/env")
+    public String getEnv() {
+        return env;
     }
 }
